a26cdffa396a09811c185cae358cd4932fe50a13
Andy Burke <andy.burke@mailbox.earth>
2020-08-24 18:09:42 -0700

    fix: ensure eslint rules pass chore: upgrade dependencies

--- test/02_alpha3.js
+++ test/02_alpha3.js
@@ -4,10 +4,10 @@ const countrydata = require( '../index.js' );
 
 const test = require( 'tape' );
 
-test( 'get by alpha3', t => {
-    countrydata.all.forEach( country => {
-        const found_country = countrydata.alpha3[ country.iso3166.alpha3 ];
-        t.deepEqual( found_country, country, `${ country.iso3166.alpha3 }: correct` );
-    } );
-    t.end();
+test( 'get by alpha3', ( t ) => {
+	countrydata.all.forEach( ( country ) => {
+		const found_country = countrydata.alpha3[ country.iso3166.alpha3 ];
+		t.deepEqual( found_country, country, `${ country.iso3166.alpha3 }: correct` );
+	} );
+	t.end();
 } );
